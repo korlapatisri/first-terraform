08f1447878793a792b3fbc02f746c92e45c68795
Shri Korlapati <shrikorlapati@Shris-MacBook-Pro.local>
2021-03-30 16:56:54 -0400

    first com

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIASAQHOYXBY6KK766L"
+    secret_key = "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"  
+}
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name: "development-vpc"
+      vpc_env: "dev"
+    }
+}
+
+resource "aws_subnet" "dev-subnet" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = "10.0.10.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name: "dev-subnet"
+    } 
+}
+
+data "aws_vpc" "existing-vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev-subnet2" {
+    vpc_id = data.aws_vpc.existing-vpc.id
+    cidr_block = "10.0.24.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name: "dev-subnet2"
+    }
+}
